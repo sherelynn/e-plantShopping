3e1a0db5c2f3958bddd8d3a126ad5815f8439e1e
sherelynn <sherelynnb@missionreadyhq.com>
2024-08-11 23:41:13 +1200

    Implement handleDecrement function to decrement the quantity or remove the item

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -26,7 +26,13 @@ const CartItem = ({ onContinueShopping }) => {
     dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }))
   }
 
-  const handleDecrement = (item) => {}
+  const handleDecrement = (item) => {
+    item.quantity > 1
+      ? dispatch(
+          updateQuantity({ name: item.name, quantity: item.quantity - 1 })
+        )
+      : dispatch(removeItem(item.name))
+  }
 
   const handleRemove = (item) => {}
 
